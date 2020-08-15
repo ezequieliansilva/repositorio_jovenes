6b294e4b8c4174c359aeb4b69afcafdf327aed8f
Ezequielian <68872000+Ezequielian@users.noreply.github.com>
2020-08-15 20:24:00 -0300

    Update entrada.js

--- js/entrada.js
+++ js/entrada.js
@@ -1,10 +1,10 @@
 
   var modal = document.getElementById("tvesModal");
-  var btn = document.getElementById("btnModal");
   var body = document.getElementsByTagName("body")[0];
   var login = document.getElementsByName("login")[0];
   var password = document.getElementsByName("password")[0];
   var btnAcceder = document.getElementById("btnAcceder");
+  var siteHeader = document.getElementsByClassName("site-header")[0];
   
   btnAcceder.onclick = function() {
 
@@ -14,6 +14,9 @@
       body.style.position = "inherit";
       body.style.height = "auto";
       body.style.overflow = "visible";
+      siteHeader.style.position = "inherit";
+      siteHeader.style.height = "auto";
+      siteHeader.style.overflow = "visible";
       }
       else {
           alert("Debe ingresar sus datos");
@@ -25,4 +28,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     body.style.position = "static";
     body.style.height = "100%";
     body.style.overflow = "hidden";
+    siteHeader.style.position = "static";
+    siteHeader.style.height = "100%";
+    siteHeader.style.overflow = "hidden";
 });

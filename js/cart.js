7e0e9ede4101e9a45d7f5ffcf337cda376a7d351
Ezequielian <68872000+Ezequielian@users.noreply.github.com>
2020-09-28 17:30:08 -0300

    entrega 5
    necesita cambios esteticos

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,55 @@
+var prodCarrito = [];
+var articulos = [];
+
+function convertir(precio , moneda ) {
+    if ( moneda != "USD"){
+        return parseInt(precio) / 40;
+    }
+    else
+    {
+        return parseInt(precio);
+    }
+};
+function printcarritou () {
+    let htmlContentToAppend = "";
+    htmlContentToAppend += `<table class="tg">
+    <thead>
+      <tr>
+        <th class="tg-0lax">Imagen</th>
+        <th class="tg-0lax">Nombre</th>
+        <th class="tg-0lax">Cantidad</th>
+        <th class="tg-0lax">Precio unitario</th>
+        <th class="tg-0lax">Precio Total</th>
+      </tr>
+    </thead>
+    <tbody>`;
+    let a
+      for(let i = 0; i < articulos.length; i++){ 
+        let unArticulo = articulos[i];
+        let convertido = convertir(unArticulo.unitCost , unArticulo.currency);
+        htmlContentToAppend += `
+      <tr>
+        <td class="tg-0lax"><img class="img-fluid img-thumbnail" src="`+ unArticulo.src + `" alt=""></td>
+        <td class="tg-0lax"><p class="mb-1">`+ unArticulo.name + `</p></td>
+        <td class="tg-0lax"><p class="mb-1">`+ unArticulo.count + `</p></td>
+        <td class="tg-0lax"><p class="mb-1"> USD `+ convertido + `</p> </td>
+        <td class="tg-0lax"><p class="mb-1"> USD `+ convertido * parseInt(unArticulo.count) + `</p></td>
+        </tr>`;
+      }
+        htmlContentToAppend +=`</tbody>
+    </table>`;
+    document.getElementById("carrito").innerHTML = htmlContentToAppend;
+};
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    getJSONData(CART_INFO2_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            prodCarrito = resultObj.data;
+            articulos = prodCarrito.articles;
+            printcarritou ();
+        }
+    });
 });

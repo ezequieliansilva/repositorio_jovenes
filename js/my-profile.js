bcdbef22a0ec7ee1cce108030aa1cc4c62174436
Ezequielian <68872000+Ezequielian@users.noreply.github.com>
2020-10-28 00:06:07 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,19 +1,19 @@
-var misDatos = [];
+var misDatos = document.getElementsByClassName("myDatos");
 
 function verificar()
 {
     let conteo = 0;
-    for (let i; i < 7 ; i++)
+    for (let i = 0; i < 7 ; i++)
     {
-        document.getElementsByClassName("datosDireccion")[i].style.backgroundColor = "white";
+        document.getElementsByClassName("myDatos")[i].style.backgroundColor = "white";
     }
-    for (let i; i < 7 ; i++)
+    for (let i = 0; i < 7 ; i++)
     {
-        if (document.getElementsByClassName("datosDireccion")[i].length < 3 && i != 1 && i != 3)
+        if (document.getElementsByClassName("myDatos")[i].value.length < 3 && i != 1 && i != 3)
         {
-            document.getElementsByClassName("datosDireccion")[i].style.backgroundColor = "pink";
+            document.getElementsByClassName("myDatos")[i].style.backgroundColor = "pink";
         }
-        else if (document.getElementsByClassName("datosDireccion")[i].length > 2 && i != 1 && i != 3)
+        else if (document.getElementsByClassName("myDatos")[i].value.length > 2 && i != 1 && i != 3)
         {
             conteo++;
         }
@@ -21,17 +21,34 @@ function verificar()
     }
     if (conteo == 5)
     {
-        alert ("Datos Guardados exitosamente");
+        alert (cargarDatos());
 
     }
     else {
-        alert ("Llene los campos restantes");
+        alert ("Llene los campos restantes "+conteo);
     }
+}
+function cargarDatos()
+{
+    for (let i = 0; i < 7; i++)
+    {
+        localStorage.setItem("dato"+i, misDatos[i].value.trim()); //setItem almacena el dato en la posición "usuario"
+    }
+    return "Datos guardados exitosamente";
+}
+
+function leerDatos(){
+    for (let i = 0; i < 7; i++)
+    {
+        var usuario = localStorage.getItem("dato"+i);
+        document.getElementsByClassName("myDatos")[i].value=usuario;
+    }
+    
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    leerDatos()
 });
